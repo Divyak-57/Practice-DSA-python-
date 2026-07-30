f991734522e678af0645be7a6a733ab14645abdf
Divya Kumari <divyakumaridk05@gmail.com>
2026-07-30 17:35:32 +0530

    Implement alternative middle node finding method
    Added an alternative approach to find the middle node of a linked list using slow and fast pointers.

--- LinkedList/876.py
+++ LinkedList/876.py
@@ -14,4 +14,15 @@ class Solution:
         for i in range(l//2): # iterate karke mid mil jaiyega
             cur = cur.next
         return cur
+
+# Another approach
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+        while(fast != None and fast.next != None):
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+        
         
