9713d3fb5b2601ab8ad982a555ae5e3c3b359d59
Divya Kumari <divyakumaridk05@gmail.com>
2026-07-30 17:32:33 +0530

    Add middleNode method to find middle of linked list
    Implement a method to find the middle node of a singly-linked list.

---
+++ LinkedList/876.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head
+        l = 0
+        while cur != None:
+            cur = cur.next
+            l+=1 # length mil jaiyega
+        cur = head 
+        for i in range(l//2): # iterate karke mid mil jaiyega
+            cur = cur.next
+        return cur
+        
